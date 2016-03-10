e9c99f41d7209e0a7d8111288ae073f47395ca41
andrewzagor <andrewzagor@gmail.com>
2016-03-11 00:29:15 +0200

    Added files via upload

---
+++ CalcSquare/RectangleSquare.java
@@ -0,0 +1,25 @@
+package Square;
+
+import java.util.Scanner;
+
+/**
+ * Created by andrew on 10.03.16.
+ */
+public class RectangleSquare {
+    static double Rectangle (double side1, double side2){
+        double square;
+        square=side1*side2;
+        return square;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.print("Введите длину первой стороны прямоугольника:");
+        Scanner sc = new Scanner(System.in);
+        double  side1 = sc.nextDouble();
+        System.out.print("Введите длину второй стороны прямоугольника:");
+        Scanner sc1 = new Scanner(System.in);
+        double  side2 = sc1.nextDouble();
+        System.out.println( Rectangle(side1,side2) );
+    }
+}
---
+++ CalcSquare/TriangleSquare.java
@@ -0,0 +1,25 @@
+package Square;
+
+import java.util.Scanner;
+
+/**
+ * Created by andrew on 10.03.16.
+ */
+public class TriangleSquare {
+    static double Triangle (double side, double high){
+        double square;
+        square=0.5*side*high;
+        return square;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.print("Введите длину стороны треугольника:");
+        Scanner sc = new Scanner(System.in);
+        double  side = sc.nextDouble();
+        System.out.print("Введите длину высоты треугольника:");
+        Scanner sc1 = new Scanner(System.in);
+        double  high = sc1.nextDouble();
+        System.out.println( Triangle(side,high) );
+    }
+}
