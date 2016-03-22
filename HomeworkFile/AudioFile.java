796687b8efe00f78485d7f4e577a8a247ad69b39
andrewzagor <andrewzagor@gmail.com>
2016-03-22 09:24:46 +0200

    Added files via upload

---
+++ HomeworkFile/AudioFile.java
@@ -0,0 +1,7 @@
+package file;
+
+/**
+ * Created by ZahornyiAI on 10.03.2016.
+ */
+public class AudioFile extends FilesCommonClass {
+}
---
+++ HomeworkFile/ImageFile.java
@@ -0,0 +1,8 @@
+package file;
+
+/**
+ * Created by ZahornyiAI on 10.03.2016.
+ */
+public class ImageFile extends FilesCommonClass {
+
+}
