19b9a955c100c1580438a21cf80a8d39e3cd5367
andrewzagor <andrewzagor@gmail.com>
2016-03-31 17:30:09 +0300

    Added files via upload

---
+++ SortCollection/Flute.java
@@ -0,0 +1,10 @@
+package CollectionMusicInstrument;
+
+/**
+ * Created by ZahornyiAI on 23.03.2016.
+ */
+public class Flute extends MusicalInstrument {
+    public Flute(String name, int quantity, int price) {
+        super(name, quantity, price);
+    }
+}
