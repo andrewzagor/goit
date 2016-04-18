113bc5438f803e95c8f6efb845f0a533bbcd45d4
andrewzagor <andrewzagor@gmail.com>
2016-04-18 15:59:33 +0300

    Added files via upload

---
+++ Project/CenterRectangleMethodTest.java
@@ -0,0 +1,40 @@
+package Project;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ZahornyiAI on 18.04.2016.
+ */
+public class CenterRectangleMethodTest {
+
+    private static   String expression="x+2";
+    public String getExpression() {
+        return expression;
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+    @Test
+    public void testCenterCornerRM() throws Exception {
+        double lowerBorder=5;
+        double upperBorder=10;
+        int countOfSteps=2;
+        CenterRectangleMethod centerRectangleMethod = new CenterRectangleMethod();
+        ConvertFunctionToRPN convertFunctionToRPN = new ConvertFunctionToRPN();
+        Double result;
+        Double deltaX = (upperBorder - lowerBorder) / countOfSteps;
+        Double[] array = new Double[countOfSteps];
+        array[0] = lowerBorder+deltaX;
+        result = deltaX * convertFunctionToRPN.calculateIntegral(expression, (array[0] - deltaX / 2));
+        for (int i = 1; i<=countOfSteps-1; i++){
+            array[i] = array[i-1]+ deltaX;
+            result = result + deltaX * convertFunctionToRPN.calculateIntegral(expression, (array[i] - deltaX / 2));
+        }
+
+        Assert.assertEquals(47.5,result,0);
+    }
+}
