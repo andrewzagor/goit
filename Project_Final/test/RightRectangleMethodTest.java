0958da7872725167e336b8929cbf090a0f9e315a
andrewzagor <andrewzagor@gmail.com>
2016-04-19 10:50:42 +0300

    Added files via upload

---
+++ Project_Final/test/RightRectangleMethodTest.java
@@ -0,0 +1,44 @@
+package ProjectNew;
+
+import Project.*;
+import Project.ConvertFunctionToRPN;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ZahornyiAI on 19.04.2016.
+ */
+public class RightRectangleMethodTest {
+    private static   String expression="x+2";
+    public String getExpression() {
+        return expression;
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+    @Test
+    public void testRightCornerRM() throws Exception {
+        double lowerBorder=5;
+        double upperBorder=10;
+        int countOfSteps=2;
+        Project.CenterRectangleMethod centerRectangleMethod = new Project.CenterRectangleMethod();
+        Project.ConvertFunctionToRPN convertFunctionToRPN = new ConvertFunctionToRPN();
+        Double result;
+        Double deltaX = (upperBorder - lowerBorder) / countOfSteps;
+        Double[] array = new Double[countOfSteps];
+        array[0] = lowerBorder+deltaX;
+        result = deltaX * convertFunctionToRPN.calculateIntegral(expression, array[0]);
+        for (int i = 1; i<=countOfSteps-1; i++){
+            array[i] = array[i-1]+ deltaX;
+            result = result + deltaX * convertFunctionToRPN.calculateIntegral(expression, array[i]);
+
+        }
+
+        double expected=53.75;
+
+        Assert.assertEquals(expected,result,0);
+    }
+}
